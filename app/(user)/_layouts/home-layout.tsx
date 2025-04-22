4a9897b092c49046aecfd81f5c2e87f5aecc42d6
zhekabaila <zheka.arkan@mimin.io>
2025-04-22 19:53:25 +0700

    feat: add

--- app/(user)/_layouts/home-layout.tsx
+++ app/(user)/_layouts/home-layout.tsx
@@ -3,20 +3,22 @@ import DataTable from '../_components/data-table'
 import { Fetcher } from '@/services/fetcher'
 import { useMediumStore } from '../_stores/use-medium-store'
 import { toast } from 'sonner'
-import { useEffect } from 'react'
-import { usePremiumStore } from '../_stores/use-premium-store'
+import { useEffect, useState } from 'react'
+// import { usePremiumStore } from '../_stores/use-premium-store'
+import { Data } from '../types'
 
 const HomeLayout = () => {
   const { medium, setMedium, loading, setLoading, fetching, setFetching } = useMediumStore()
-  const {
-    premium,
-    setPremium,
-    loading: premiumLoading,
-    setLoading: setPremiumLoading,
-    fetching: premiumFetching,
-    setFetching: setPremiumFetching
-  } = usePremiumStore()
-
+  // const {
+  //   premium,
+  //   setPremium,
+  //   loading: premiumLoading,
+  //   setLoading: setPremiumLoading,
+  //   fetching: premiumFetching,
+  //   setFetching: setPremiumFetching
+  // } = usePremiumStore()
+  // const [rataRata, setRataRata] = useState(0)
+  const [prediksi, setPrediksi] = useState<number[]>([])
   const fetchMedium = () => {
     if (!loading) setFetching(true)
 
@@ -24,6 +26,8 @@ const HomeLayout = () => {
       url: '/medium',
       onSuccess({ data }) {
         setMedium(data)
+        const r = getPrediksi(data)
+        setPrediksi(r)
       },
       onError(error) {
         toast.error(error)
@@ -35,33 +39,53 @@ const HomeLayout = () => {
     })
   }
 
-  const fetchPremium = () => {
-    if (!premiumLoading) setPremiumFetching(true)
+  // const fetchPremium = () => {
+  //   if (!premiumLoading) setPremiumFetching(true)
 
-    Fetcher({
-      url: '/premium',
-      onSuccess({ data }) {
-        setPremium(data)
-      },
-      onError(error) {
-        toast.error(error)
-      },
-      onFinally() {
-        if (premiumLoading) setPremiumLoading(false)
-        else setPremiumFetching(false)
-      }
-    })
+  //   Fetcher({
+  //     url: '/premium',
+  //     onSuccess({ data }) {
+  //       setPremium(data)
+  //     },
+  //     onError(error) {
+  //       toast.error(error)
+  //     },
+  //     onFinally() {
+  //       if (premiumLoading) setPremiumLoading(false)
+  //       else setPremiumFetching(false)
+  //     }
+  //   })
+  // }
+
+  const getPrediksi = (data: Data[]) => {
+    let totalPerubahan = 0
+    for (let i = 1; i < data.length; i++) {
+      totalPerubahan += data[i].price - data[i - 1].price
+    }
+    const rataRata = totalPerubahan / data.length - 1
+    const prediksi = []
+    for (let i = 0; i < 12; i++) {
+      const bulanSebelumnya = data[data.length - 1].price // Harga bulan terakhir
+      prediksi[i] = bulanSebelumnya + rataRata * (i + 1)
+    }
+    return prediksi
   }
 
   useEffect(() => {
     fetchMedium()
-    fetchPremium()
+    // fetchPremium()
   }, [])
 
   return (
     <div className="m-10">
       <DataTable data={medium} loading={loading} fetching={fetching} />
-      <DataTable data={premium} loading={premiumLoading} fetching={premiumFetching} />
+      {/* <div className="text-center text-2xl font-bold">Rata-rata perubahan harga: {rataRata}</div> */}
+      {/* <DataTable data={premium} loading={premiumLoading} fetching={premiumFetching} /> */}
+      {prediksi.map((p, i) => (
+        <div key={i} className="text-center text-2xl font-bold">
+          Prediksi harga beras hari ke-{i + 1}: {p}
+        </div>
+      ))}
     </div>
   )
 }

ee23ae3affd0723fb37e7baadc6d4850bd90d4d7
zhekabaila <zheka.arkan@mimin.io>
2025-05-07 11:02:10 +0700

    feat: add

--- app/(user)/_components/bar-chart.tsx
+++ app/(user)/_components/bar-chart.tsx
@@ -10,12 +10,12 @@ export default function BarChartComponent({ chartRef, isPrediksi }: BarChartProp
     <div className="w-full h-[800px] bg-secondary rounded-md p-6">
       <div className="flex items-center justify-center gap-4">
         <div className="flex items-center gap-2">
-          <div className="bg-primary border border-primary w-8 h-3"></div>
+          <div className="bg-[#7c8755] border border-[#7c8755] w-8 h-3"></div>
           <p className="text-sm">Data</p>
         </div>
         {isPrediksi && (
           <div className="flex items-center gap-2">
-            <div className="bg-primary/40 border border-primary w-8 h-3"></div>
+            <div className="bg-[#667b99] border border-[#667b99] w-8 h-3"></div>
             <p className="text-sm">Prediksi</p>
           </div>
         )}
